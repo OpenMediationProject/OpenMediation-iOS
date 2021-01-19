e4da90866837d41d52aca547b1bc2431c7fd4446
yinliming <liam.yin@adtiming.com>
2021-01-19 16:58:55 +0800

    V2.0.5

--- OpenMediation/OpenMediationConstant.h
+++ OpenMediation/OpenMediationConstant.h
@@ -4,7 +4,7 @@
 #ifndef OpenMediationConstant_h
 #define OpenMediationConstant_h
 
-#define OPENMEDIATION_SDK_VERSION    @"2.0.3"
+#define OPENMEDIATION_SDK_VERSION    @"2.0.5"
 
 typedef NS_ENUM(NSInteger, OMAdNetwork) {
     OMAdNetworkAdTiming = 1,
