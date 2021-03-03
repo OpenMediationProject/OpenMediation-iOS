26a6fe10362a46c65f837480a62b630ddb47abcc
yinliming <liam.yin@adtiming.com>
2021-03-03 11:13:04 +0800

    KuaiShou Adapter

--- OMKuaiShouAdapter/OMKuaiShouAdapterClass.h
+++ OMKuaiShouAdapter/OMKuaiShouAdapterClass.h
@@ -4,6 +4,9 @@
 #ifndef OMKuaiShouAdapterClass_h
 #define OMKuaiShouAdapterClass_h
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
 typedef NS_ENUM(NSInteger, KSAdSDKLogLevel) {
     KSAdSDKLogLevelAll      =       0,
     KSAdSDKLogLevelVerbose,  // 此类别的日记不会记录到日志文件中
@@ -18,6 +21,55 @@ typedef NS_ENUM(NSInteger, KSAdSDKLogLevel) {
 @interface KSAdSDKManager : NSObject
 @property (nonatomic, readonly, class) NSString *SDKVersion;
 + (void)setAppId:(NSString *)appId;
+@end
+
+typedef NS_ENUM(NSInteger, KSAdShowDirection) {
+    KSAdShowDirection_Vertical         =           0,
+    KSAdShowDirection_Horizontal,
+};
+
+
+typedef NS_ENUM(NSInteger, KSAdInteractionType) {
+    KSAdInteractionType_Unknown,
+    KSAdInteractionType_App,
+    KSAdInteractionType_Web,
+    KSAdInteractionType_DeepLink,
+};
+
+
+@interface KSAd : NSObject
+
+// 单位:分，只有视频资源下载成功后，这个才可能有值
+@property (nonatomic, readonly) NSInteger ecpm;
+/// 媒体二次议价, 单位分
+- (void)setBidEcpm:(NSInteger)ecpm;
+
+@end
+
+@interface KSVideoAd : KSAd
+
+@property (nonatomic, readonly) BOOL isValid;
+@property (nonatomic, assign) BOOL shouldMuted;
+
+- (void)loadAdData;
+// direction 默认是 Vertical
+- (BOOL)showAdFromRootViewController:(UIViewController *)rootViewController;
+- (BOOL)showAdFromRootViewController:(UIViewController *)rootViewController direction:(KSAdShowDirection)direction;
+// direction 默认是 Vertical
+- (BOOL)showAdFromRootViewController:(UIViewController *)rootViewController showScene:(nullable NSString *)showScene;
+- (BOOL)showAdFromRootViewController:(UIViewController *)rootViewController showScene:(nullable NSString *)showScene direction:(KSAdShowDirection)direction;
+
+
+
+/*
+ 这个是播放异常的时候,此方法不会自动调用，可以在
+ - (void)rewardedVideoAdDidPlayFinish:(KSRewardedVideoAd *)rewardedVideoAd didFailWithError:(NSError *_Nullable)error使用此方法
+ */
+- (void)closeVideoAdWhenPlayError;
+
+// 是否是同一个有效广告
+- (BOOL)isSameValidVideoAd:(nullable KSVideoAd *)ad;
+
 @end
 
 #endif /* OMKuaiShouAdapterClass_h */
--- OMKuaiShouAdapter/OMKuaiShouRewardedVideoClass.h
+++ OMKuaiShouAdapter/OMKuaiShouRewardedVideoClass.h
@@ -5,57 +5,9 @@
 #define OMKuaiShouRewardedVideoClass_h
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import "OMKuaiShouAdapter.h"
 
 NS_ASSUME_NONNULL_BEGIN
-
-typedef NS_ENUM(NSInteger, KSAdShowDirection) {
-    KSAdShowDirection_Vertical         =           0,
-    KSAdShowDirection_Horizontal,
-};
-
-
-typedef NS_ENUM(NSInteger, KSAdInteractionType) {
-    KSAdInteractionType_Unknown,
-    KSAdInteractionType_App,
-    KSAdInteractionType_Web,
-    KSAdInteractionType_DeepLink,
-};
-
-
-@interface KSAd : NSObject
-
-// 单位:分，只有视频资源下载成功后，这个才可能有值
-@property (nonatomic, readonly) NSInteger ecpm;
-/// 媒体二次议价, 单位分
-- (void)setBidEcpm:(NSInteger)ecpm;
-
-@end
-
-@interface KSVideoAd : KSAd
-
-@property (nonatomic, readonly) BOOL isValid;
-@property (nonatomic, assign) BOOL shouldMuted;
-
-- (void)loadAdData;
-// direction 默认是 Vertical
-- (BOOL)showAdFromRootViewController:(UIViewController *)rootViewController;
-- (BOOL)showAdFromRootViewController:(UIViewController *)rootViewController direction:(KSAdShowDirection)direction;
-// direction 默认是 Vertical
-- (BOOL)showAdFromRootViewController:(UIViewController *)rootViewController showScene:(nullable NSString *)showScene;
-- (BOOL)showAdFromRootViewController:(UIViewController *)rootViewController showScene:(nullable NSString *)showScene direction:(KSAdShowDirection)direction;
-
-
-
-/*
- 这个是播放异常的时候,此方法不会自动调用，可以在
- - (void)rewardedVideoAdDidPlayFinish:(KSRewardedVideoAd *)rewardedVideoAd didFailWithError:(NSError *_Nullable)error使用此方法
- */
-- (void)closeVideoAdWhenPlayError;
-
-// 是否是同一个有效广告
-- (BOOL)isSameValidVideoAd:(nullable KSVideoAd *)ad;
-
-@end
 
 @class KSRewardedVideoModel;
 @protocol KSRewardedVideoAdDelegate;
